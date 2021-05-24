72983afd2f6963186ff06af0e7b4adf41db275b4
Matías Otermín <mati_otermin@yahoo.com>
2021-05-24 14:50:32 -0300

    Listo el cacheo de consultas

--- src/main/java/Clima/ServicioMeteorologicoAccuWeatherApi.java
+++ src/main/java/Clima/ServicioMeteorologicoAccuWeatherApi.java
@@ -1,15 +1,38 @@
 package Clima;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServicioMeteorologicoAccuWeatherApi implements ProveedorClima {
 
-  AccuWeatherAPI miApi = new AccuWeatherAPI();
+  private AccuWeatherAPI miApi;
+  private Map<String, Object> ultimasRespuestas;
+  private Duration periodoDeValidez;
+  private LocalDateTime proximaExpiracion;
+
+  public ServicioMeteorologicoAccuWeatherApi() {
+    this.miApi = new AccuWeatherAPI();
+    this.ultimasRespuestas = new HashMap<String, Object>();
+    this.periodoDeValidez = Duration.ofMinutes(150);
+    this.proximaExpiracion = LocalDateTime.now();
+  }
 
   public Map<String, Object> climaSegunAPISeleccionada() {
-    //miApi.controlarCantidadMaximaDeConsultas();
-    return miApi.getWeather("Buenos Aires, Argentina").get(0);
+    if(this.expiro()) {
+      this.ultimasRespuestas = miApi.getWeather("Buenos Aires, Argentina").get(0);
+      this.proximaExpiracion = this.proximaExpiracion();
+    }
+    return ultimasRespuestas;
+  }
+
+  private LocalDateTime proximaExpiracion() {
+    return LocalDateTime.now().plus(this.periodoDeValidez);
+  }
+
+  private boolean expiro() {
+    return proximaExpiracion.isBefore((LocalDateTime.now()));
   }
 
   public double getTemperatura(){
